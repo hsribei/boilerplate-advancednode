6c720e935d4eddf8a93f820417e211ad3dcbfe8d
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-03-13 17:36:50 -0400

    Set up express-session and passport

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+fccTesting(app); //For FCC testing purposes
 
 // Pretty logging when in dev
 if (process.env.NODE_ENV === "development") {
@@ -13,9 +17,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set("view engine", "pug");
 
-fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
